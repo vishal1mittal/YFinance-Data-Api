838dcf9bb025e8c5feaf298f6136838b0af09dee
Vishal Mittal <vishalmittalrohini@gmail.com>
2024-02-24 02:09:05 +0530

    Created Historical Data Route as well as Token authentication

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const yahooFinance = require("yahoo-finance2");
+const yahooFinance = require("yahoo-finance2").default;
 const jwt = require("jsonwebtoken");
 const rateLimit = require("express-rate-limit");
+require("dotenv").config();
 
 // Set up rate limiter
 const limiter = rateLimit({
@@ -16,15 +17,11 @@ const app = express();
 app.use(cors());
 app.use(limiter);
 
-// Configure Yahoo Finance
-// yahooFinance.setOptions({
-//   modules: ["quote"],
-//   range: "1d",
-// });
-
-// Generate a JWT token
+// Generate a Json Web Token(JWT) token
 function generateToken(username) {
-  return jwt.sign({ username }, "secret_key", { expiresIn: "1h" });
+  return jwt.sign({ username }, process.env.JWT_Token_SECRET_KEY, {
+    expiresIn: "365d",
+  });
 }
 
 // Verify a JWT token
@@ -33,7 +30,7 @@ function verifyToken(req, res, next) {
   if (!token) return res.status(401).send("Access denied.");
 
   try {
-    const decoded = jwt.verify(token, "secret_key");
+    const decoded = jwt.verify(token, process.env.JWT_Token_SECRET_KEY);
     req.user = decoded;
     next();
   } catch (ex) {
@@ -41,21 +38,35 @@ function verifyToken(req, res, next) {
   }
 }
 
-// Get stock quote
+// Get historical stock data
 app.get("/quote/:symbol", verifyToken, async (req, res) => {
   const symbol = req.params.symbol;
+  const from = new Date(req.query.from);
+  const to = new Date(req.query.to);
+  const interval = req.query.interval;
+  const queryOptions = { period1: from, period2: to, interval: interval };
 
   try {
-    const quote = await yahooFinance.quote({ symbol });
-    res.json(quote);
+    const data = await yahooFinance.historical(symbol, queryOptions);
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
 // Generate a new token
-app.post("/token", (req, res) => {
-  const username = req.body.username;
+app.post("/generatetoken", (req, res) => {
+  const username = req.query.username;
+  if (
+    typeof username !== "string" ||
+    username === undefined ||
+    username === null ||
+    username.length < 3
+  ) {
+    return res
+      .status(400)
+      .json({ message: "Username must be at least 3 characters long." });
+  }
   const token = generateToken(username);
   res.json({ token });
 });
